fa540e10a824329b699ff2a69ec11ce07a863126
lasaro <lasaro@here>
2021-12-13 11:29:32 -0300

    def locais

--- src/Exercise.hs
+++ src/Exercise.hs
@@ -1,5 +1,4 @@
 module Exercise where
-import Data.Char (isUpper,isLower,toUpper,toLower,isLetter)
 
 -- Defina as seguintes funções usando let in e where para fazer definições locais de testes de condições.
 -- Inclua os tipos.
@@ -7,10 +6,11 @@ import Data.Char (isUpper,isLower,toUpper,toLower,isLetter)
 
 imc :: Float -> Float -> String
 imc p a
-    | imc' <= baixo = "Baixo"
+    | imc' <= baixo  = "Baixo"
     | imc' <= normal = "Normal"
-    | imc' <= alto = "Alto"
-    where imc' = p/a^2
+    | imc' <= alto   = "Alto"
+    | otherwise     = "Altíssimo"
+    where imc' = p/a**2
           baixo = 18.5
           normal = 25.0
           alto = 30.0
@@ -35,4 +35,23 @@ inverteCase
 ' '
 
 -}
-inverteCase c = undefined 
+inverteCase :: p -> a
+inverteCase c = undefined
+
+
+{-
+Esta função retorna a representação textual de uma data. Isto é, dados dia, mês e ano em sua forma numérica, retorna a data por extenso.
+
+Use ++ para concatenar duas Strings.
+Use show para gerar uma string a partir de um número.
+>>>dataPorExtenso 10 10 2021
+"dez de Outubro de 2021"
+
+>>>dataPorExtenso 1 1 1900
+"primeiro de Janeiro de 1900"
+
+-}
+
+dataPorExtenso d m a = diaPorExtenso d ++ " de " ++ mêsPorExtenso m ++ " de " ++ show a
+    where diaPorExtenso dia = undefined
+          mêsPorExtenso mmes = undefined 
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,7 +1,9 @@
-import Test.Hspec        (Spec, it, describe, shouldBe, anyErrorCall, shouldThrow)
+import Test.Hspec        (Spec, it, describe, shouldBe, anyErrorCall, anyException, shouldThrow)
 import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
 import Test.QuickCheck
 import Control.Exception.Base
+
+import Data.Char (isUpper,isLower,toUpper,toLower,isLetter)
 
 import Exercise
 
@@ -10,36 +12,22 @@ main = hspecWith defaultConfig {configFastFail = False} specs
 
 specs :: Spec
 specs = do
-          describe "Pedra Tesoura Papel" $ do
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapelPadrões  0 1 `shouldBe` True
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapelPadrões  1 0 `shouldBe` False
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapelPadrões  0 2 `shouldBe` False
-            it "Pedra Tesoura Papel" $
-              pedraTesouraPapelPadrões  2 0 `shouldBe` True
-
-          describe "diasMesPadrões" $ do
-            it "Janeiro" $ do
-              diasMesPadrões 1 `shouldBe` 31
-            it "Dezembro" $ do
-              diasMesPadrões 12 `shouldBe` 31
-            it "Novembro" $ do
-              diasMesPadrões 11 `shouldBe` 30
-            it "Negativo" $ do
-              evaluate (diasMesPadrões (-1)) `shouldThrow` anyErrorCall
-            it ">12" $ do
-              evaluate (diasMesPadrões (13)) `shouldThrow` anyErrorCall
-
-          describe "eLógico" $ do
+          describe "Inverter case" $ do
+            it "minúsculo" $
+              inverteCase 'a' `shouldBe` 'A'
+            it "maiúsculo" $
+              inverteCase 'A' `shouldBe` 'a'
+            it "nada" $
+              inverteCase ' ' `shouldBe` ' '
+            it "ponto" $
+              inverteCase '.' `shouldBe` '.'
             it "Testa todas as combinações " $ property $
-              \x y -> eLógico x y `shouldBe` x && y
+              \x -> inverteCase x `shouldBe` if isLetter x then if isLower x then toUpper x else toLower x else x
 
-          describe "ouLógico" $ do
-            it "Testa todas as combinações " $ property $
-              \x y -> ouLógico x y `shouldBe` x || y
-
-          describe "éVogal'" $ do
-            it "Testa muitos caracteres " $ property $
-              \x -> éVogal x `shouldBe` x `elem` "AEIOUaeiou"
+          describe "data por extenso" $ do
+            it "1 do 1 do 1" $ do
+              dataPorExtenso 1 1 1901 `shouldBe` "primeiro de Janeiro de 1901"
+            it "2 do 2 do 2" $ do
+              dataPorExtenso 2 2 2002 `shouldBe` "dois de Fevereiro de 2002"
+            it "1 1 do 0" $ do
+              dataPorExtenso 1 1 2000 `shouldBe` "primeiro de Janeiro de 2000"
